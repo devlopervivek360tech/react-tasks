5ea7030c46d58b98edcf47b0944256832d1c9b70
Om Pimpale <ompimpale2003@gmail.com>
2025-04-22 17:56:24 +0530

    task of user card & responsive navbar using MUI is done

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -1,7 +1,8 @@
 import '../css/Users.css'
 import Header from '../Header';
+import user from '../utils/user';
 
-let Users = () =>{
+let Users = () => {
 
     const role = {
         admin: [
@@ -11,15 +12,28 @@ let Users = () =>{
         ]
     }
 
-    return(
+    return (
 
         <>
-        <Header sendData={role.admin}/>
-        <section>
-            <div className="userdiv">
-                <h1 id="userHead">Users</h1>
-            </div>
-        </section>
+            <Header sendData={role.admin} />
+            <section className='mx-4'>
+                <div className="userdiv my-5">
+                    <h1 id="userHead">Users</h1>
+                </div>
+                <div className='row justify-content-evenly container-fluid'>
+                    {user.map((user,id)=>(
+                        <div className="card col-12 col-md-5 col-lg-3 p-2 mx-2 my-3" key={id}>
+                        <img src={user.avatar} className="card-img-top img-fluid" alt={"Avtar"+user.id} />
+                        <div className="card-body">
+                            <h4 className="card-title">{user.profession}</h4>
+                            <p className='card-name mb-0'>{user.name}</p>
+                            <h6 className='mb-2'><a href="#" className='card-mail text-decoration-none'>{user.email}</a></h6>
+                            <p className="card-text">{user.description}</p>
+                        </div>
+                    </div>
+                    ))}
+                </div>
+            </section>
         </>
 
     )
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,39 +1,78 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import { Link, Toolbar, AppBar } from '@mui/material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBarsStaggered } from '@fortawesome/free-solid-svg-icons';
 import './Header.css';
-let Header = ({ sendData =[] }) => {
-    // sendData = []
-    return (
 
-        <>
-            <section id='navbar'>
-                <div>
-                    <section>
-                        <div id='navdiv'>
-                            <div>
-                                <h1 id='logo'><Link to={"/"}>Logo</Link></h1>
-                            </div>
-                            <div>
-                                <div id='linkdiv'>
-                                    <ul id='navlinks'>
-                                        <li className='links'><Link to={"/"} className='othernavanchor'>Home</Link></li>
-                                        {sendData.map((link, index) => (
-                                            <li key={index} className='links'><Link to={link.path} className='navanchor'>{link.label}</Link></li>
-                                        ))}
-                                        <li className='links'><Link to={"/user"} className='othernavanchor'>User</Link></li>
-                                        <li className='links'><Link to={"/admin"} className='othernavanchor'>Admin</Link></li>
-                                        <li className='links'><Link to={"/manager"} className='othernavanchor'>Manager</Link></li>
-                                        <li className='links'><Link to={"/logout"} className='othernavanchor'>Logout</Link></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-                </div>
-            </section>
-        </>
+const Header = ({ sendData = [] }) => {
+  const [showNav, setShowNav] = useState(false);
 
-    )
+  const toggleNav = () => {
+    setShowNav(na => !na);
+  };
 
-}
+  return (
+    <>
+      <AppBar position="fixed" sx={{ backgroundColor: '#bbdefb' }}>
+        <Toolbar disableGutters>
+          <div id='navdiv' className='d-flex justify-content-between w-100 mx-3'>
+            {/* Logo */}
+            <div>
+              <h1 id='logo'>
+                <Link component={RouterLink} to="/" color="inherit">Logo</Link>
+              </h1>
+            </div>
+
+            {/* Hamburger for small screens */}
+            <div className='d-lg-none'>
+              <Link onClick={toggleNav}>
+                <FontAwesomeIcon icon={faBarsStaggered} color='#0000ff' />
+              </Link>
+            </div>
+
+            {/* Nav for large screens */}
+            <div className='d-none d-lg-inline-block bar'>
+              <div id='linkdiv'>
+                <ul id='navlinks'>
+                  <li className='links'><Link component={RouterLink} to="/" color="inherit" className='othernavanchor'>Home</Link></li>
+                  {sendData.map((link, index) => (
+                    <li key={index} className='links'>
+                      <Link component={RouterLink} to={link.path} className='navanchor'>{link.label}</Link>
+                    </li>
+                  ))}
+                  <li className='links'><Link component={RouterLink} to="/user" color="inherit" className='othernavanchor'>User</Link></li>
+                  <li className='links'><Link component={RouterLink} to="/admin" color="inherit" className='othernavanchor'>Admin</Link></li>
+                  <li className='links'><Link component={RouterLink} to="/manager" color="inherit" className='othernavanchor'>Manager</Link></li>
+                </ul>
+              </div>
+            </div>
+
+            <div className='d-none d-lg-inline-block bar'>
+              <Link component={RouterLink} to="/logout" color="inherit" className='othernavanchor m-0 text-danger'>Logout</Link>
+            </div>
+          </div>
+        </Toolbar>
+      </AppBar>
+
+      {showNav && (
+        <div className='d-lg-none bg-light px-4 pt-2 pb-3 mt-5'>
+          <ul className='list-unstyled pt-4 mb-0'>
+            <li><Link component={RouterLink} to="/" className='d-block py-1 navanchor'>Home</Link></li>
+            {sendData.map((link, index) => (
+              <li key={index}>
+                <Link component={RouterLink} to={link.path} className='d-block py-1 navanchor'>{link.label}</Link>
+              </li>
+            ))}
+            <li><Link component={RouterLink} to="/user" className='d-block py-1 navanchor'>User</Link></li>
+            <li><Link component={RouterLink} to="/admin" className='d-block py-1 navanchor'>Admin</Link></li>
+            <li><Link component={RouterLink} to="/manager" className='d-block py-1 navanchor'>Manager</Link></li>
+            <li><Link component={RouterLink} to="/logout" className='d-block py-1 text-danger'>Logout</Link></li>
+          </ul>
+        </div>
+      )}
+    </>
+  );
+};
 
 export default Header;
